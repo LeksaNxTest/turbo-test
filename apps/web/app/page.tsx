b6af2af6fbf38e177e28da503b9e80a26078368a
Aleksa S. Stevic <aleksa.stevic26@gmail.com>
2024-02-22 16:56:00 +0100

    feat: add new text

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Page() {
 
   return (
     <div>
-      <h1>My page</h1>
+      <h1>My page (ADD NEW TEXT)</h1>
       <p>API_URL: {process.env.API_URL}</p>
       <p>Message from API: {response.message}</p>
     </div>
